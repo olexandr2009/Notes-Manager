36c4a85715f1d6d0f198e274ec2d67f8cbe9923e
olexandr2009 <hristevich.ua@gmail.com>
2023-11-26 14:35:15 +0200

    Pilot

--- src/main/java/com/example/goit/crudservice/CrudService.java
+++ src/main/java/com/example/goit/crudservice/CrudService.java
@@ -5,9 +5,9 @@ import com.example.goit.note.Note;
 import java.util.List;
 
 public interface CrudService<T> {
-    List<Note> listAll();
-    Note add(Note note);
+    List<T> listAll();
+    T add(T t);
     void deleteById(long id) throws ObjectNotFoundException;
-    void update(Note note) throws ObjectNotFoundException;
-    Note getById(long id) throws ObjectNotFoundException;
+    void update(T note) throws ObjectNotFoundException;
+    T getById(long id) throws ObjectNotFoundException;
 }
